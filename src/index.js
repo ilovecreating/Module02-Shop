c861daa368b00a85f714766fbd0e86f8e581d2f1
borntocreate <traceon2016@gmail.com>
2023-01-07 23:27:15 +0300

    COMMIT!!!

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
 import 'normalize.css'
 import './styles/main.scss'
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
